b066796701a36166fcdc099e27385d05f05a4c51
João Henrique <santos.joao@crmsolucoes.com.br>
2023-10-09 08:51:57 -0300

    6 - Abstração

---
+++ 6 - ABSTRAÇÃO/index.php
@@ -0,0 +1,40 @@
+<?php
+
+abstract class Banco{
+
+    protected $saldo;
+    protected $limiteSaque;
+    protected $juros;
+
+    public function setSaldo($s){
+        $this->saldo = $s;
+    }
+
+    public function getSaldo(){
+        return $this->saldo;
+    }
+
+    abstract protected function Sacar($s);
+    abstract protected function Depositar($s);
+
+}
+
+class Itau extends Banco{
+
+    public function Sacar ($s){
+        $this->saldo -= $s;
+    }
+
+    public function Depositar ($s){
+        $this->saldo += $s;
+    }
+}
+
+$itau = new Itau();
+$itau->setSaldo(1000);
+
+echo "<hr> Saldo: ".$itau->getSaldo();
+$itau->Sacar(250);
+echo "<hr> Saldo: ".$itau->getSaldo();
+
+?>
