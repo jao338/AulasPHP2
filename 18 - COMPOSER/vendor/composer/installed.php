729f4ad8d7c6c20bb3733ff309dc8216c55f6b80
João Henrique <santos.joao@crmsolucoes.com.br>
2023-10-10 08:57:49 -0300

    18 - Composer

---
+++ 18 - COMPOSER/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aulasphp2/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3ea741bfe3b73c8842295f8d37920bf2a65537d8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aulasphp2/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3ea741bfe3b73c8842295f8d37920bf2a65537d8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pagseguro/pagseguro-php-sdk' => array(
+            'pretty_version' => '4.2.1',
+            'version' => '4.2.1.0',
+            'reference' => '3e164aff66fa089eb79a80ce6b7413fe36aa9b95',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pagseguro/pagseguro-php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
